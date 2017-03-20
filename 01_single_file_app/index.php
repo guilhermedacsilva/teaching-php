435c87bb1e772d54c7b3807af9c0ed72134d529d
dev <remotedeveloper88@gmail.com>
2017-03-20 15:51:06 -0300

    Project 1: chat online with less than 50 lines of code.

---
+++ 01_single_file_app/index.php
@@ -0,0 +1,48 @@
+<?php
+define('DATAFILE', 'data.txt');
+
+if (!file_exists(DATAFILE)) {
+    $emptyArraySerialized = serialize([]);
+    file_put_contents(DATAFILE, $emptyArraySerialized);
+}
+
+$needCreate = array_key_exists('userName', $_POST) && array_key_exists('userMessage', $_POST);
+if ($needCreate) {
+    $stringData = file_get_contents(DATAFILE);
+    $arrayData = unserialize($stringData);
+    $arrayData[] = [
+        'userName' => $_POST['userName'],
+        'userMessage' => $_POST['userMessage'],
+    ];
+    $stringData = serialize($arrayData);
+    file_put_contents(DATAFILE, $stringData);
+}
+
+$stringData = file_get_contents(DATAFILE);
+$arrayData = unserialize($stringData);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Chat Online</title>
+    <style type="text/css">
+        .bold { font-weight: bold; }
+    </style>
+</head>
+<body>
+    <h1>Send your message</h1>
+    <form method="post">
+        User: <input name="userName">
+        Message: <textarea name="userMessage"></textarea>
+        <input type="submit" value="Send" name="submit">
+    </form>
+    <h1>Old messages</h1>
+    <?php foreach ($arrayData as $message) : ?>
+        <p>
+            <span class="bold"><?= $message['userName'] ?></span>:
+            <?= $message['userMessage'] ?>
+        </p>
+    <?php endforeach ?>
+</body>
+</html>
